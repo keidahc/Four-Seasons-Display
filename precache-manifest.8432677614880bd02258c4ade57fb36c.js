c48d334b5512f5e1c1dcd792b2a0de22b7ec5979
keidahc <54549505+keidahc@users.noreply.github.com>
2019-09-27 12:29:54 -0700

    Updates

---
+++ precache-manifest.8432677614880bd02258c4ade57fb36c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94af3ea16ce5af86afe0cb0c42a25706",
+    "url": "/Four-Seasons-Display/index.html"
+  },
+  {
+    "revision": "7ec199e8fe7843c13042",
+    "url": "/Four-Seasons-Display/static/css/main.ab8a8906.chunk.css"
+  },
+  {
+    "revision": "a75eb4f3f2667a7d1084",
+    "url": "/Four-Seasons-Display/static/js/2.e90dc5dc.chunk.js"
+  },
+  {
+    "revision": "7ec199e8fe7843c13042",
+    "url": "/Four-Seasons-Display/static/js/main.99043d56.chunk.js"
+  },
+  {
+    "revision": "55649c0bb9bfd8aeedc6",
+    "url": "/Four-Seasons-Display/static/js/runtime~main.1af09a8e.js"
+  }
+]);
